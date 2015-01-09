20e25919245a991463c48502e9337b24dd96dce0
n321203 <n321203@gmail.com>
2015-01-09 13:21:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,430 @@
+var Resource = require('deployd/lib/resource')
+  , Script = require('deployd/lib/script')
+  , UserCollection = require('deployd/lib/resources/user-collection')
+  , internalClient = require('deployd/lib/internal-client')
+  , util = require('util')
+  , url = require('url')
+  , debug = require('debug')('dpd-passport')
+
+  // Stetegies
+  , LocalStrategy = require('passport-local').Strategy
+  , TwitterStrategy = require('passport-twitter').Strategy
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , GitHubStrategy = require('passport-github').Strategy
+
+  // Globals
+  , DEFAULT_SALT_LEN = 256
+  , CALLBACK_URL = 'callback';
+
+function AuthResource() {
+    Resource.apply(this, arguments);
+
+    // read and parse config
+    var config = this.config;
+    config.SALT_LEN = config.SALT_LEN || DEFAULT_SALT_LEN;
+    config.baseURL = config.baseURL || process.env.DPD_PASSPORT_BASEURL;
+
+    config.allowTwitter = config.allowTwitter && config.baseURL && config.twitterConsumerKey && config.twitterConsumerSecret;
+    config.allowFacebook = config.allowFacebook && config.baseURL && config.facebookAppId && config.facebookAppSecret;
+    config.allowGitHub = config.allowGitHub && config.baseURL && config.githubClientId && config.githubClientSecret;
+}
+util.inherits(AuthResource, Resource);
+
+AuthResource.label = "Passport-Auth";
+AuthResource.defaultPath = '/auth';
+module.exports = AuthResource;
+
+AuthResource.prototype.clientGeneration = false;
+
+AuthResource.prototype.initPassport = function() {
+    if(this.initialized) return;
+
+    var config = this.config,
+        dpd = internalClient.build(process.server, {isRoot: true}, []),
+        userCollection = process.server.resources.filter(function(res) {return res.config.type === 'UserCollection'})[0],
+        passport = (this.passport = require('passport'));
+
+    // Will be called when socialLogins are done
+    // Check for existing user and update
+    // or create new user and insert
+    var socialAuthCallback = function(token, tokenSecret, profile, done) {
+        debug('Login callback - profile: %j', profile);
+
+        userCollection.store.first({socialAccountId: profile.id}, function(err, user) {
+            if(err) { return done(err); }
+
+            // Patch: Always update name/ID/provider from facebook, not only on registration
+            var saveUser = {
+                socialAccountId: profile.id,
+                socialAccount: profile.provider,
+                name: profile.displayName
+            }
+            //var saveUser = user || {
+                // these properties will only be set on first insert
+                // socialAccountId: profile.id,
+                // socialAccount: profile.provider,
+                // name: profile.displayName
+            //};
+
+            // update the profile on every login, so that we always have the latest info available
+            saveUser.profile = profile;
+
+            if(user) {
+                debug('updating existing user w/ id', user.id);
+            } else {
+                debug('creating new user w/ socialAccountId=%s', saveUser.socialAccountId);
+            }
+
+            /* we need to fake the password here, because deployd will force us to on create
+            // but we'll clear that later
+            saveUser.username = saveUser.socialAccount + '_' + saveUser.socialAccountId;
+            saveUser.password = saveUser.username;
+            */
+
+            // PATCH: We try to fetch user email from facebook and store it as username!            
+            if(typeof(profile.emails) == "object" && profile.emails[0].value){
+                saveUser.username = profile.emails[0].value; // Email from facebook
+                saveUser.password = "" + Math.round(Math.random()*10000000000000);
+
+                // If user is already registered (with username+password), update that user instead of creating new
+                dpd.users.get({username:saveUser.username}, function(users, err){
+                    if(err)
+                        return done(err)
+
+                    if(users && users.length > 0){  // Username is already registered
+                        saveUser.id = users[0].id;  // Store id, that way dpd will update this user, not create a new 
+                        delete saveUser.password;   // User exists, so don't overwrite existing password
+                    }
+
+                    // Add new or update existing user
+                    dpd.users.put(saveUser, function(res, err){
+                        if(err)
+                            return done(err);
+                        else 
+                            done(null, res||saveUser)
+                    })
+                })
+            }
+            else{
+                // Facebook didn't provide a user email. Let's store a default email and password
+                saveUser.username = profile.provider + '_' + profile.id + "@example.com";
+                saveUser.password = "" + Math.round(Math.random()*10000000000000);
+
+                dpd.users.put(saveUser, function(res, err){
+                    if(err)
+                        return done(err);
+                    else 
+                        done(null, res||saveUser)
+                })                
+            }
+        });
+    };
+
+    if(config.allowLocal) {
+        passport.use(new LocalStrategy(
+          function(username, password, done) {
+            userCollection.store.first({username: username}, function(err, user) {
+                if(err) { return done(err); }
+
+                if(user) {
+                    var salt = user.password.substr(0, config.SALT_LEN)
+                      , hash = user.password.substr(config.SALT_LEN);
+
+                    if(hash === UserCollection.prototype.hash(password, salt)) {
+                        return done(null, user);
+                    }
+                }
+
+                return done(null, false, { message: 'Invalid password' });
+            });
+          }
+        ));
+    }
+
+    if(config.allowTwitter) {
+        var cbURL = url.resolve(config.baseURL, this.path + '/twitter/' + CALLBACK_URL);
+
+        debug('Initializing Twitter Login, cb: %s', cbURL);
+        passport.use(new TwitterStrategy({
+            consumerKey: config.twitterConsumerKey,
+            consumerSecret: config.twitterConsumerSecret,
+            callbackURL: cbURL,
+            sessionKey: 'data'
+          },
+          socialAuthCallback
+        ));
+    }
+
+    if(config.allowFacebook) {
+        var cbURL = url.resolve(config.baseURL, this.path + '/facebook/' + CALLBACK_URL);
+        
+        debug('Initializing Facebook Login, cb: %s', cbURL);
+        passport.use(new FacebookStrategy({
+            clientID: config.facebookAppId,
+            clientSecret: config.facebookAppSecret,
+            callbackURL: cbURL,
+            authorizationURL: "https://www.facebook.com/v2.0/dialog/oauth" // ADDED: Move to Graph API 2.0
+          },
+          socialAuthCallback
+        ));
+    }
+
+    if(config.allowGitHub) {
+        var cbURL = url.resolve(config.baseURL, this.path + '/github/' + CALLBACK_URL);
+
+        debug('Initializing GitHub Login, cb: %s', cbURL);
+        passport.use(new GitHubStrategy({
+            clientID: config.githubClientId,
+            clientSecret: config.githubClientSecret,
+            callbackURL: cbURL
+          },
+          socialAuthCallback
+        ));
+    }
+
+    this.initialized = true;
+}
+
+var sendResponse = function(ctx, err, session) {
+    if(ctx.session.data.redirectURL) {
+        var redirectURL = ctx.session.data.redirectURL;
+        // delete search so that query is used
+        delete redirectURL.search;
+
+        // make sure query is inited
+        redirectURL.query = redirectURL.query || {};
+
+        if(err) {
+            redirectURL.query.success = false;
+            redirectURL.query.error = err;
+        } else {
+            // append user + session id to the redirect url
+            redirectURL.query.success = true;
+            redirectURL.query.sid = session.id;
+            redirectURL.query.uid = session.uid;
+        }
+
+        var redirectURLString = '';
+        try {
+            redirectURLString = url.format(redirectURL);
+        } catch(ex) {
+            console.warn('An error happened while formatting the redirectURL', ex);
+        }
+
+        // redirect the user
+        ctx.res.setHeader("Location", redirectURLString);
+        ctx.res.statusCode = 302;
+        
+        ctx.done(null, 'This page has moved to ' + redirectURLString);
+    } else {
+        if(err) {
+            ctx.res.statusCode = 401;
+            return ctx.done('bad credentials');
+        } else {
+            ctx.done(err, session);
+        }
+    }
+}
+AuthResource.prototype.handle = function (ctx, next) {
+    // globally handle logout
+    if(ctx.url === '/logout') {
+        if (ctx.res.cookies) ctx.res.cookies.set('sid', null, {overwrite: true});
+        ctx.session.remove(ctx.done);
+        return;
+    }
+
+    var parts = ctx.url.split('/').filter(function(p) {
+        // filters out all empty parts
+        return p;
+    });
+
+    // determine requested module
+    var requestedModule, options = {};
+
+       switch(parts[0]) {
+
+        case 'login':
+            if(ctx.method === 'POST' && this.config.allowLocal) {
+                requestedModule = 'local';
+            }
+            break;
+        case 'twitter':
+            if(this.config.allowTwitter) {
+                requestedModule = 'twitter';
+            }
+            break;
+        case 'facebook':
+            if(this.config.allowFacebook) {
+                requestedModule = 'facebook';
+                if(this.config.facebookScope) {
+                    try {
+                        options.scope = JSON.parse(this.config.facebookScope);
+                    } catch(ex) {
+                        debug('Error parsing the facebookScope');
+                    }
+                }
+            }
+            break;
+        case 'github':
+            if(this.config.allowGitHub) {
+                requestedModule = 'github';
+                if(this.config.githubScope) {
+                    try {
+                        options.scope = JSON.parse(this.config.githubScope);
+                    } catch(ex) {
+                        debug('Error parsing the githubScope')
+                    }
+                }
+            }
+            break;
+        case 'loginWithToken': // Patch: added login with token
+            requestedModule = "loginWithToken";
+            break;
+        default:
+            break;
+    }
+
+    if(requestedModule) {
+        // logout the user if he was logged in before, but only if that's not a callback
+        if(ctx.session && !(parts && parts.length >= 2 && parts[1] === CALLBACK_URL)) {
+            debug('Cleaning leftover session. %j', parts);
+            // throw away the old session
+            ctx.session.remove(function() {
+                // ignore the callback here, this is just for cleanup
+            });
+
+            // this is not actually async, as we don't supply an existing sid
+            process.server.sessions.createSession(function(err, sess) {
+                ctx.session = sess;
+            });
+
+            if (ctx.res.cookies) {
+                ctx.res.cookies.set('sid', ctx.session.sid, {overwrite: true});
+            }
+        }
+
+        // save the redirectURL for later use
+        if(ctx.query.redirectURL && this.config.allowedRedirectURLs) {
+            try {
+                this.regEx = this.regEx || new RegExp(this.config.allowedRedirectURLs, 'i');
+                if(ctx.query.redirectURL.match(this.regEx)) {
+
+                    // save this info into the users session, so that we can access it later (even if the user was redirected to facebook)
+                    ctx.session.data.redirectURL = url.parse(ctx.query.redirectURL, true);
+                } else {
+                    debug(ctx.query.redirectURL, 'did not match', this.config.allowedRedirectURLs);
+                }
+            } catch(ex) {
+                debug('Error parsing RedirectURL Regex!', ex);
+            }
+        }
+
+
+        this.initPassport();
+
+        // Patch – added loginWithToken – login using username+token
+        if(requestedModule == "loginWithToken"){
+
+            var username    = (ctx.req.query.username || ctx.req.body.username)
+            var token       = (ctx.req.query.token || ctx.req.body.token)
+            var userCollection = process.server.resources.filter(function(res) {return res.config.type === 'UserCollection'})[0];
+
+            userCollection.store.first({username: username}, function(err, user) {
+                if(err)
+                    return sendResponse(ctx, err, session);
+                if(user){
+
+                    var hash = userCollection.hash(username, "348ea2d3fc8b3a46f86b8dc37a9377107e255c8e");
+                    if(token === hash){
+                        ctx.session.set({path: '/users', uid: user.id}).save(function(err, session){
+                            return sendResponse(ctx, err, session);
+                        })
+                    }
+                    else return sendResponse(ctx, 'bad credentials');
+                }
+                else return sendResponse(ctx, 'bad credentials');
+            })
+            return;
+        }
+
+        // Usual logins...
+        this.passport.authenticate(requestedModule, options, function(err, user, info) {
+            if (err || !user) {
+                debug('passport reported error: ', err, user, info);
+                return sendResponse(ctx, 'bad credentials');
+            }
+
+            ctx.session.set({path: '/users', uid: user.id}).save(function(err, session) {
+                return sendResponse(ctx, err, session);
+            });
+        })(ctx.req, ctx.res, ctx.next||ctx.done);
+    } else {
+        // nothing matched, sorry
+        debug('no module found: ', parts[0]);
+        return sendResponse(ctx, 'bad credentials');
+    }
+};
+
+AuthResource.basicDashboard = {
+  settings: [{
+    name        : 'SALT_LEN',
+    type        : 'numeric',
+    description : 'Length of the Password salt that is used by deployd. Do not change if you don\'t know what this is or your users may not login anymore! Defaults to 256.'
+  },{
+    name        : 'baseURL',
+    type        : 'text',
+    description : 'Specify the Base URL of your site (http://www.your-page.com/) that is used for callbacks. *Required when using any OAuth Login!* Defaults to env variable DPD_PASSPORT_BASEURL.'
+  },{
+    name        : 'allowedRedirectURLs',
+    type        : 'text',
+    description : 'Specify a regular expression for which redirect URLs you want to allow. Supply as JS-Regex: "^http://www\.your-page.com/.*$", matching is always done case-insensitive. Defaults to "" (i.e. NO redirects will be allowed!)'
+  },{
+    name        : 'allowLocal',
+    type        : 'checkbox',
+    description : 'Allow users to login via Username + Password'
+  },{
+    name        : 'allowTwitter',
+    type        : 'checkbox',
+    description : 'Allow users to login via Twitter (requires Twitter Key and Secret!)'
+  },{
+    name        : 'allowFacebook',
+    type        : 'checkbox',
+    description : 'Allow users to login via Facebook (requires Facebook Id and Secret!)'
+  },{
+    name        : 'allowGitHub',
+    type        : 'checkbox',
+    description : 'Allow users to login via GitHub (requires GitHub Id and Secret!)'
+  },{
+    name        : 'twitterConsumerKey',
+    type        : 'text'/*,
+    description : 'TWITTER_CONSUMER_KEY'*/
+  }, {
+    name        : 'twitterConsumerSecret',
+    type        : 'text'/*,
+    description : 'TWITTER_CONSUMER_SECRET'*/
+  },{
+    name        : 'facebookAppId',
+    type        : 'text'/*,
+    description : 'TWITTER_CONSUMER_KEY'*/
+  }, {
+    name        : 'facebookAppSecret',
+    type        : 'text'/*,
+    description : 'TWITTER_CONSUMER_SECRET'*/
+  }, {
+    name        : 'facebookScope',
+    type        : 'text',
+    description : 'If your application needs extended permissions, they can be requested here. Supply as JS-Array: "[\'read_stream\']"'
+  }, {
+    name        : 'githubClientId',
+    type        : 'text'/*,
+    description : 'TODO'*/
+  }, {
+    name        : 'githubClientSecret',
+    type        : 'text'/*,
+    description : 'TODO'*/
+  }, {
+    name        : 'githubScope',
+    type        : 'text',
+    description : 'If your application needs extended permissions, they can be requested here. Supply as JS-Array: "[\'repo\']"'
+  }]
+};
